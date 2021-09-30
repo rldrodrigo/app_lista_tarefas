4289327efc9ed0919115e612d8dc22830a542362
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-09-30 12:10:04 -0300

    Update banco_de_dados.sql

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -13,3 +13,9 @@ create table tb_tarefas(
 	tarefa text not null,
     data_cadastrado datetime not null default current_timestamp
 )
+CREATE TABLE IF NOT EXISTS tb_usuarios(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(32) NOT NULL
+)
