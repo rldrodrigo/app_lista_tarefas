d3415f142bb74de115358880de0068a1f057e98a
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-09-30 16:17:57 -0300

    Criando a classe Tarefa
    Scripts públicos farão require dos scripts protegidos, será usado uma pasta_privada para simular

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -12,10 +12,11 @@ create table tb_tarefas(
     foreign key(id_status) references tb_status(id),
 	tarefa text not null,
     data_cadastrado datetime not null default current_timestamp
-)
+);
+
 CREATE TABLE IF NOT EXISTS tb_usuarios(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(32) NOT NULL
-)
+);
